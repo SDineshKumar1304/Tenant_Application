3fd7c7bce62f485b8185e103cd953aa82407601b
S. Dinesh Kumar <125432987+SDineshKumar1304@users.noreply.github.com>
2024-08-01 19:50:17 +0530

    Delete app.py

--- app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, render_template, request, redirect, url_for, flash, session
-from werkzeug.utils import secure_filename
-import os
-
-app = Flask(__name__)
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
